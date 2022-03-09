d4f8a84b1c1542e7c83f4b6a8a341a4eed618db4
cytusalive <cytusomega@gmail.com>
2022-03-08 23:32:03 -0800

    added button

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const sayHello = function() {
+    alert('You clicked me!');
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={sayHello}>Click Me</button>;
       </header>
     </div>
   );
